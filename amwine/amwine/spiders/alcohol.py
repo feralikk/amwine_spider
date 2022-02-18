dc2182cc230bbf49ab9c22fd4ae6a9db77e2fc85
feralikk <66134845+feralikk@users.noreply.github.com>
2022-02-18 17:04:40 +0500

    Update alcohol.py

--- amwine/amwine/spiders/alcohol.py
+++ amwine/amwine/spiders/alcohol.py
@@ -6,26 +6,37 @@ import time
 class AlcoholSpider(scrapy.Spider):
     name = 'alcohol'
     allowed_domains = ['amwine.ru']
-    start_urls = ['https://amwine.ru/catalog/krepkie_napitki/konyak/a-de-fussigny-selection/']
-    page_count = 4
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+    start_urls = ['https://amwine.ru/catalog/krepkie_napitki/']
+    category_list = ['konyak', 'vodka']
+    section_id_list = [18, 29]
     headers = {
         'accept': '*/*',
         'accept-encoding': 'gzip, deflate, br',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+        'user_agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Mobile Safari/537.36'
     }
 
-    def start_requests(self):
-        """ Отправляет HTTP запрос на сервер."""
+
+    def start_request(self):
+        mainpage_url = "https://amwine.ru/catalog/krepkie_napitki/konyak/"
+        scrapy.Request(mainpage_url,
+                       callback=self.parse
+                       )
+
+    def parse(self, response):
+        page_count = response.css('ul.catalog-pagination li::text')[5].get().strip()
+        int(page_count)
         request_url = 'https://amwine.ru/local/components/adinadin/catalog.section.json/ajax_call.php'
-        for page in range(1, self.page_count):
-            yield scrapy.Request(request_url,
-                                 body=f'json=y&params%5BIBLOCK_TYPE%5D=catalog&params%5BIBLOCK_ID%5D=2&params%5BCACHE_TYPE%5D=Y&params%5BCACHE_TIME%5D=3600&params%5BSECTION_ID%5D=18&params%5BSECTION_CODE%5D=konyak&params%5BPRICE_CODE%5D=MOSCOW&params%5BPAGE_ELEMENT_COUNT%5D=18&params%5BFILTER_NAME%5D=arrFilterCatalog&params%5BSORT_ORDER%5D=ASC&params%5BSORT_FIELD%5D=SORT&params%5BMESSAGE_404%5D=&params%5BSET_STATUS_404%5D=&params%5BSHOW_404%5D=Y&params%5BFILE_404%5D=&params%5BNO_INDEX_NO_FOLLOW%5D=N&params%5BCURRENT_PAGE%5D=1&params%5B~IBLOCK_TYPE%5D=catalog&params%5B~IBLOCK_ID%5D=2&params%5B~CACHE_TYPE%5D=Y&params%5B~CACHE_TIME%5D=3600&params%5B~SECTION_ID%5D=18&params%5B~SECTION_CODE%5D=konyak&params%5B~PRICE_CODE%5D=MOSCOW&params%5B~PAGE_ELEMENT_COUNT%5D=18&params%5B~FILTER_NAME%5D=arrFilterCatalog&params%5B~SORT_ORDER%5D=ASC&params%5B~SORT_FIELD%5D=SORT&params%5B~MESSAGE_404%5D=&params%5B~SET_STATUS_404%5D=&params%5B~SHOW_404%5D=Y&params%5B~FILE_404%5D=&params%5B~NO_INDEX_NO_FOLLOW%5D=N&params%5B~CURRENT_PAGE%5D=1&current_filter%5BACTIVE%5D=Y&current_filter%5BIBLOCK_ID%5D=2&current_filter%5BINCLUDE_SUBSECTIONS%5D=Y&current_filter%5BSECTION_ID%5D=18&PAGEN_1={page}&sort=sort',
-                                 headers={'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
-                                 method='POST',
-                                 callback=self.parse_request
-                                 )
+        for i in range(2):
+            for page in range(1, page_count):
+                category = self.category_list[i]
+                section_id = self.section_id_list[i]
+                yield scrapy.Request(request_url,
+                                     body=f'json=y&params%5BIBLOCK_TYPE%5D=catalog&params%5BIBLOCK_ID%5D=2&params%5BCACHE_TYPE%5D=Y&params%5BCACHE_TIME%5D=3600&params%5BSECTION_ID%5D={section_id}&params%5BSECTION_CODE%5D={category}&params%5BPRICE_CODE%5D=MOSCOW&params%5BPAGE_ELEMENT_COUNT%5D=18&params%5BFILTER_NAME%5D=arrFilterCatalog&params%5BSORT_ORDER%5D=ASC&params%5BSORT_FIELD%5D=SORT&params%5BMESSAGE_404%5D=&params%5BSET_STATUS_404%5D=&params%5BSHOW_404%5D=Y&params%5BFILE_404%5D=&params%5BNO_INDEX_NO_FOLLOW%5D=N&params%5BCURRENT_PAGE%5D=1&params%5B~IBLOCK_TYPE%5D=catalog&params%5B~IBLOCK_ID%5D=2&params%5B~CACHE_TYPE%5D=Y&params%5B~CACHE_TIME%5D=3600&params%5B~SECTION_ID%5D={section_id}&params%5B~SECTION_CODE%5D={category}&params%5B~PRICE_CODE%5D=MOSCOW&params%5B~PAGE_ELEMENT_COUNT%5D=18&params%5B~FILTER_NAME%5D=arrFilterCatalog&params%5B~SORT_ORDER%5D=ASC&params%5B~SORT_FIELD%5D=SORT&params%5B~MESSAGE_404%5D=&params%5B~SET_STATUS_404%5D=&params%5B~SHOW_404%5D=Y&params%5B~FILE_404%5D=&params%5B~NO_INDEX_NO_FOLLOW%5D=N&params%5B~CURRENT_PAGE%5D=1&current_filter%5BACTIVE%5D=Y&current_filter%5BIBLOCK_ID%5D=2&current_filter%5BINCLUDE_SUBSECTIONS%5D=Y&current_filter%5BSECTION_ID%5D=18&PAGEN_1={page}&sort=sort',
+                                     headers={'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                                     method='POST',
+                                     callback=self.parse_request
+                                     )
 
     def parse_request(self, response):
         """Парсит url-ссылку на товар."""
@@ -51,10 +62,12 @@ class AlcoholSpider(scrapy.Spider):
             sections.append(section)
 
         current_price = response.css('.catalog-element-info__price_detail span:not([class])::text').get().replace(" ", "")
+        current_price = current_price.replace(u'\xa0', u' ')
         current_price = float(current_price)
         original_price = (response.css('div.catalog-element__wrap span::text').get())
         if original_price is not None:
-            original_price.replace(" ", "")
+            original_price = original_price.replace(" ", "")
+            original_price = original_price.replace(u'\xa0', u' ')
             original_price = float(original_price)
             proc = (current_price / original_price) * 100
         else:
@@ -78,16 +91,24 @@ class AlcoholSpider(scrapy.Spider):
         discription_right = ''
         i = 0
         for params in response.css('div.about-wine__block div.h4::text').getall():
-            discription_right_title.append(params.strip())
+            params.strip()
+            if params.find(':') == -1:
+                params += ":"
+            discription_right_title.append(params)
         for params1 in response.css('div.about-wine__block p::text').getall():
-            discription_right += discription_right_title[i] + " " + params1.strip()
+            params1.strip()
+            if params1.find('\n') == 0:
+                i -= 1
+                discription_right += " " + params1 + "; "
+            else:
+                discription_right += discription_right_title[i] + " " + params1 + "; "
             i += 1
 
         item = {
             'timestamp': time.time(),  # Текущее время в формате timestamp
             'RPC': response.css('div.catalog-element-info__article span::text').get().replace(" ", ""),
             # {str} Уникальный код товара
-            'url': response.request.url,  # {str} Ссылка на страницу товара
+            'url': str(response.request.url),  # {str} Ссылка на страницу товара
             'title': response.css('div.catalog-element-info__title h1::text').get().strip(),
             # {str} Заголовок/название товара
             'brand': brand,  # {str} Брэнд товара
@@ -98,8 +119,8 @@ class AlcoholSpider(scrapy.Spider):
                 'sale_tag': f'Скидка {proc}%',  # {str} Скидка
             },
             'stock': stock,  # {bool} Должно отражать наличие товара в магазине
-            'main_image': url_image,  # {str} Ссылка на основное изображение товара
-            "description": discription_right,
+            'main_image': str(url_image),  # {str} Ссылка на основное изображение товара
+            "description": discription_right
             # {str} Описание товар Ниже добавить все характеристики которые могут быть на
             # странице тоавара, такие как Артикул, Код товара, Цвет, Объем, Страна производитель и т.д.
         }
